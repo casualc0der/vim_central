cf65fac53d6e927f7e17f84a838e7eeed2fb5222
casualc0der <edwardsansome@gmail.com>
2020-12-19 23:52:26 +0000

    add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+Cool theme [gruvbox](https://github.com/morhetz/gruvbox/blob/master/colors/gruvbox.vim)
+Add vim to loadpath to allow modules to be added [pathogen](https://github.com/tpope/vim-pathogen)
+Sensible defaults for vim [sensible-vim](https://github.com/tpope/vim-sensible)
+Spec runner for vim [rspec-vim](https://github.com/thoughtbot/vim-rspec)
+Use 'gcc' when in normal or visual mode to comment code! [vim-commentary](https://github.com/tpope/vim-commentary)
+Auto complete (requires solargraph gem for Ruby) [coc-vim](https://github.com/neoclide/coc-solargraph)
+Add a status bar at the bottom of the terminal [vim-airline](https://github.com/vim-airline/vim-airline)
+
+
+we should really write a script that will install all of the above...
--- spec/wow_spec.rb
+++ spec/wow_spec.rb
@@ -6,11 +6,17 @@ class Wow
   end
 
   def hello
-    'gday mate'
+    'gday mate'.encode
   end
 
   def goodbye
+    return 'goodbye pal' if @file.empty? || @file.is_a?(Array)
+
     "goodbye #{@file}"
+  end
+  def filter
+    return :rejected if @file.include?('fick')
+    :ok
   end
 end
 
@@ -27,6 +33,18 @@ RSpec.describe 'Wow' do
     let(:file) { 'Eddie' }
     it 'see above' do
       expect(subject.goodbye).to eq('goodbye Eddie')
+    end
+  end
+  context 'ok it can do one more thing' do
+    let(:file) { "" }
+    it 'calls the user "pal" if no name is supplied' do
+      expect(subject.goodbye).to eq('goodbye pal')
+    end
+  end
+  context 'maybe one more before bed!' do
+    let(:file) { ['fick', 'hello', 'jelly'] }
+    it 'rejects a file if it includes a naughty word' do
+      expect(subject.filter).to eq(:rejected)
     end
   end
 end
